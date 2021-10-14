642b588e5c37c9e5fe283ba8fac07bef2fe1ec3e
yasexxx <69728632+yasexxx@users.noreply.github.com>
2021-10-14 11:58:54 +0800

    adding new deploy files

--- src/main/java/com/thinkifyapi/app/restapithinkify/RestapiThinkifyApplication.java
+++ src/main/java/com/thinkifyapi/app/restapithinkify/RestapiThinkifyApplication.java
@@ -1,7 +1,7 @@
 package com.thinkifyapi.app.restapithinkify;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class RestapiThinkifyApplication {
